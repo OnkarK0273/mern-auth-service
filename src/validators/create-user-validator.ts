c9ce534736f20fa25f1b5bb5be7d4104b2804970
Onkar k <onkarok0273.1@gmail.com>
2026-01-30 19:15:49 +0530

    feat: add password field validator

--- src/validators/create-user-validator.ts
+++ src/validators/create-user-validator.ts
@@ -19,4 +19,13 @@ export default checkSchema({
     notEmpty: true,
     trim: true,
   },
+  password: {
+    errorMessage: 'Password is required',
+    notEmpty: true,
+    trim: true,
+    isLength: {
+      options: { min: 8 },
+      errorMessage: 'Password should be at least 8 chars',
+    },
+  },
 });
